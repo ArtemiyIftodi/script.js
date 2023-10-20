bb201e550c914fc9766b688dde26a93fae451d97
ArtemiyIftodi <148565106+ArtemiyIftodi@users.noreply.github.com>
2023-10-20 18:28:21 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,21 +5,12 @@ $start.addEventListener('click', startGame)
 
 
 function startGame() {
-    $game.style.baclgroundColor = '#fff'
-    $start.classList.add('hide')
+  $game.style.backgroundColor = '#fff'
+  $start.classList.add('hide')
 
-    renderBox()
+  renderBox()
 }
 
 function renderBox() {
-  var box = document.createElement('div')
-  box.style.height = box.style.width = '50px'
-  box.style.position = 'absolute'
-  box.style.baclgroundColor = '#000'
-  box.style.top = '50px'
-  box.style.left = '70px'
-  box.style.cursor = 'pointer'
 
-
-  $game.insertAdjacentElement('afterbegin', box)
 }
