5f7f27395ad95e9e188649dab9b396b76d45a978
ArtemiyIftodi <148565106+ArtemiyIftodi@users.noreply.github.com>
2023-10-20 18:25:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var $start = document.querySelector('#start')
+var $game = document.querySelector('#game')
+
+$start.addEventListener('click', startGame)
+
+
+function startGame() {
+    $game.style.baclgroundColor = '#fff'
+    $start.classList.add('hide')
+
+    renderBox()
+}
+
+function renderBox() {
+  var box = document.createElement('div')
+  box.style.height = box.style.width = '50px'
+  box.style.position = 'absolute'
+  box.style.baclgroundColor = '#000'
+  box.style.top = '50px'
+  box.style.left = '70px'
+  box.style.cursor = 'pointer'
+
+
+  $game.insertAdjacentElement('afterbegin', box)
+}
